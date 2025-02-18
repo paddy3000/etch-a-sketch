463ebdc16356c4439f9ec06ff95c9fb30b2981f2
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-02-18 18:55:38 +0000

    feat: colour changing added

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 const container=document.querySelector(".all-squares");
 
-for (let i = 0; i < 16; i++) {
-    node=document.createElement("div");
-    node.setAttribute("class", "square square-default")
-    container.appendChild(node);
+changeColour=function(box){
+    if (box.getAttribute("class")==="square blue") {
+        box.setAttribute("class", "square pink");
+    } else {
+        box.setAttribute("class", "square blue");
 }
+}
+
+createSquares = function(rows){
+  for (let i = 1; i <= rows; i++) {
+    row=document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("id", "row"+i);
+      for (let j = 1; j<=rows; j++){
+        node=document.createElement("div");
+        node.setAttribute("class", "square blue");
+        node.setAttribute("id", "square"+i+"_"+j);
+        row.appendChild(node);
+      }
+    container.appendChild(row);
+  }
+
+  const allSquares=document.querySelectorAll(".square");
+
+  allSquares.forEach(square => {
+    square.addEventListener("mouseenter", function(e) {
+        console.log(e.target.getAttribute("id"))
+        changeColour(e.target);
+    })
+});
+}
+
+createSquares(4);
+
+const button=document.querySelector("button");
+
+button.addEventListener("click", function(e){
+    rows=prompt("How many rows?");
+
+    if (rows>100){
+        rows=100;
+        alert("100 rows is maximum allowed");
+    }
+    container.innerHTML="";
+    createSquares(rows);
+
+    changeColour(e.target);
+})
